4c36a092d50748ad0a3c8b41d9ae581379483607
James Chiang <jchiang87@gmail.com>
2024-04-10 10:13:09 -0700

    instrument the DRP datacat query to check for empty return values

--- pipeline_scripts/drp_datacat_query.sh
+++ pipeline_scripts/drp_datacat_query.sh
@@ -10,5 +10,12 @@ time_query="(${opt1} || ${opt2} || ${opt3})"
 (/sdf/home/g/glast/a/datacat/prod/datacat find --group FT2 --filter "$time_query" ${folder}) > ${OUTPUTDIR}/Ft2FileList
 
 logical_path=${pgwave_folder}/${frequency}
+file_list=${OUTPUTDIR}/pgwaveFileList
 pgw_query="nDatasetId==${pgwave_streamId} && $time_query"
-(/sdf/home/g/glast/a/datacat/prod/datacat --mode DEV find --group PGWAVESOURCELIST --filter "$pgw_query" ${logical_path}) > ${OUTPUTDIR}/pgwaveFileList
+(/sdf/home/g/glast/a/datacat/prod/datacat --mode DEV find --group PGWAVESOURCELIST --filter "$pgw_query" ${logical_path}) > ${file_list}
+
+if [ -z "$(cat ${file_list})" ]
+then
+    echo "File ${file_list} is empty."
+    exit 1
+fi
